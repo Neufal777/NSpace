5d1a0a0031c34a8b81a3091b9866c3904d10d97e
neufal777 <neufal777@gmail.com>
2020-03-26 22:17:21 +0100

    saving changes

--- main.go
+++ main.go
@@ -2,24 +2,55 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
 	"net/http"
 )
 
-func index_handler(w http.ResponseWriter, r *http.Request) {
-
-	fmt.Fprintf(w, "HOME")
+type notification struct {
+	read   int32
+	unread int32
 }
 
-func run_profile(w http.ResponseWriter, r *http.Request) {
+type user struct {
+	Name          string
+	Surname       string
+	Nickname      string
+	notifications notification
+	balance       float32
+}
 
-	fmt.Fprintf(w, "PROFILE")
+func runHomePage(w http.ResponseWriter, r *http.Request) {
+
+	user := []user{
+		{
+			Name:     "Naoufal",
+			Surname:  "Dahouli",
+			Nickname: "neufal79",
+			notifications: notification{
+				read:   2,
+				unread: 32,
+			},
+			balance: 4.8,
+		},
+		{
+			Name:     "Marshall",
+			Surname:  "Mathers",
+			Nickname: "mm89",
+			notifications: notification{
+				read:   5,
+				unread: 12,
+			},
+			balance: 5.7,
+		},
+	}
+
+	t, _ := template.ParseFiles("templates/home.html")
+	t.Execute(w, user[1])
 
 }
 
 func main() {
 
-	http.HandleFunc("/", index_handler)
-	http.HandleFunc("/profile", run_profile)
+	http.HandleFunc("/", runHomePage)
 	http.ListenAndServe(":8000", nil)
 }
